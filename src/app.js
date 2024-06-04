2cec8221b75a1546f875be21e2670a2c0add9144
Yash Lokhande <91784682+ylokhande2003@users.noreply.github.com>
2024-06-04 14:50:16 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: 'https://665ebf604dcb48d1b11319c7--incomparable-stardust-31e43f.netlify.app',
+    origin: 'https://665ebf604dcb48d1b11319c7--incomparable-stardust-31e43f.netlify.app' || 'http://localhost:5173',
+    credentials: true,
 }
   
 ))
