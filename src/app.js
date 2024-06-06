7d8d4cb07fd4164e380ad156fdadd8e5f5e5a9f8
Yash Lokhande <91784682+ylokhande2003@users.noreply.github.com>
2024-06-06 16:18:14 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,15 +7,7 @@ const app = express()
 
 
 
-app.use(cors({
-    origin: [
-  'https://6661918181dc9f07e9ad0d1c--idyllic-rabanadas-3ef716.netlify.app/',
-  'http://localhost:5173'
-],
-    credentials: true
-}
-  
-))
+app.use(cors())
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
